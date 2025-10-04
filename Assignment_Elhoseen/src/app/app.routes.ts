9676039b637338297e8944d16830e678bfe4f214
Diaa Mohammed <diaam852@gmail.com>
2025-10-04 15:00:02 +0300

    add guard for products url to check if authnticated or no

--- Assignment_Elhoseen/src/app/app.routes.ts
+++ Assignment_Elhoseen/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { authGuard } from './core/auth.guard';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'products', component: ProductListComponent }
+  { path: 'products', component: ProductListComponent ,canActivate:[authGuard]}
 ];
